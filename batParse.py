c1494262daaf91d40a20970f2acce10469144ebe
Marco Koch <marco@5th.ch>
2019-04-22 14:52:29 +0100

    skip printing the lines with twice the date in it and no data

--- batParse.py
+++ batParse.py
@@ -1,11 +1,13 @@
 import sys, re, os
 
-
-time = ''
-
 with open(sys.argv[1]) as f:  
     for line in f:
         ## regex not matching negative timezones
         match = re.search('\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2}', line)
-        time = match.group() if match else time
-        print '{},{}'.format(time, line)
+
+        if match:
+            # found date but theres no data on this line skip printing this line
+            date = match.group()
+            continue
+        else:
+            print '{},{}'.format(date, line)
